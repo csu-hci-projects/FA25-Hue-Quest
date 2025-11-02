fdf55c7de4828c63a7382c3299578cff0d7c642c
Peri Grover <peri@colostate.edu>
2025-11-01 18:40:33 -0600

    Astronaut and movement script

---
+++ Assets/Stylized_Astronaut/Character/ColorManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    [SerializeField] public bool hasRed = false;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
