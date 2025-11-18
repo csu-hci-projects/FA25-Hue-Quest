2da6895e1b87290d7cb5d7af369fe12c4c1c9fd6
Prajjol Neupae <prajjol2014@gmail.com>
2025-11-18 13:21:21 -0700

    worked on greenlevel

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit Button");
+    }
+}
