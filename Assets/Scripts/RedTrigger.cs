dd0d02a20fd56d2d3675ab5cb6b68910a8f74c65
Peri Grover <peri@colostate.edu>
2025-11-18 12:06:01 -0700

    Red Trigger

---
+++ Assets/Scripts/RedTrigger.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class RedTrigger : MonoBehaviour
+{
+    public Material redMat;
+    public Material brownMat;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Red Paint is grabbed");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player has picked up red paint");
+            ColorManager playerColorManager = other.gameObject.GetComponent<ColorManager>();
+            playerColorManager.hasRed = true;
+            Debug.Log("Player should now have red and can jump.");
+            Debug.Log("Adding colors...");
+            AddRedToLvl();
+            Debug.Log("Colors should be added.");
+        }
+        gameObject.SetActive(false);
+    }
+
+    void AddRedToLvl()
+    {
+
+        GameObject[] redObjects = GameObject.FindGameObjectsWithTag("IsRed");
+        GameObject[] brownObjects = GameObject.FindGameObjectsWithTag("IsBrown");
+        if (redObjects.Length > 0)
+        {
+            Debug.Log("red objects found");
+            foreach (GameObject redobj in redObjects)
+            {
+                redobj.GetComponent<MeshRenderer>().material = redMat;
+            }
+            Debug.Log("all red objects found should now be red.");
+        }
+        if (brownObjects.Length > 0)
+        {
+            Debug.Log("brown objs found");
+            foreach (GameObject brnobj in brownObjects)
+            {
+                brnobj.GetComponent<MeshRenderer>().material = brownMat;
+            }
+            Debug.Log("all brown objs found should now be brown.");
+        }
+        return;
+    }
+}
