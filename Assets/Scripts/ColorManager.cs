eda1e2787235ee23d0b6ca0a9151f978488d5ef6
tyler <treichma@colostate.edu>
2025-12-01 16:41:09 -0700

    OrangeLevel update 1

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -5,10 +5,11 @@ public class ColorManager : MonoBehaviour
     [SerializeField] public bool hasBlue = false;
     [SerializeField] public bool hasRed = false;
     [SerializeField] public bool hasGreen = false;
-    [SerializeField] public bool hasOrnge = false;
     [SerializeField] public bool hasYellow = false;
     [SerializeField] public bool hasPurpule = false;
-    
+    [SerializeField] public bool hasOrange = false;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,6 +18,6 @@ public class ColorManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
