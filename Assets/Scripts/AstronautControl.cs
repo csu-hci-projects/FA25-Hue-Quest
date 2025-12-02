1ff6bff493c48a9e65227f378c31d8be715009c0
Prajjol Neupae <prajjol2014@gmail.com>
2025-12-01 17:54:14 -0700

    removed leftover header

--- Assets/Scripts/AstronautControl.cs
+++ Assets/Scripts/AstronautControl.cs
@@ -2,13 +2,9 @@ using UnityEngine;
 
 public class AstronautControl : MonoBehaviour
 {
-<<<<<<<< HEAD:Assets/Scripts/AstronautControl.cs
-========
     [SerializeField] public bool hasRed = false;
     [SerializeField] public bool hasOrange = false;
 
-
->>>>>>>> d54fc5a5bd9d6f81f304e73fda6611c3fbfa6dfd:Assets/Stylized_Astronaut/Character/ColorManager.cs
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
