1907641ef4a062d57a93996a6300da88eb287346
Peri Grover <peri@colostate.edu>
2025-12-01 18:29:01 -0700

    Red Level Update

---
+++ Assets/Scripts/LavaController.cs
@@ -0,0 +1,38 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LavaController : MonoBehaviour
+{
+    private bool isRising = false;
+    public float riseSpeed = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRising)
+        {
+            transform.position += Vector3.up * riseSpeed * Time.deltaTime;
+        }
+    }
+
+    public void startRising()
+    {
+        isRising = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player has hit lava, player should now be dead");
+            //Add death scene script here?
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
