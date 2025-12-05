2aae004a34671088a9956da738ff06a029eeae63
tyler <treichma@colostate.edu>
2025-12-05 01:39:48 -0700

    fixed shaders and crashing problem, few updates

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -142,7 +142,10 @@ public class ThirdPersonMovement : MonoBehaviour
         //movement 
         //Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            anim.SetTrigger("Woah");
+        }
 
 
         //add jumping
