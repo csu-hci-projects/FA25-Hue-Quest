0bacd103783eed7a2c49a698eb53e0d77ffc78c1
Prajjol Neupae <prajjol2014@gmail.com>
2025-12-05 15:16:56 -0700

    finished green level and made blue level

--- Assets/Scripts/GreenTrigger.cs
+++ Assets/Scripts/GreenTrigger.cs
@@ -5,22 +5,32 @@ public class GreenTrigger : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] Terrain terrain;
     [SerializeField] ColorManager player;
+    [SerializeField] AudioManager audioManager;
+    [SerializeField] AudioClip startSong;
+    [SerializeField] AudioClip greenSong;
     void Start()
     {
-    }
+        if (player.hasGreen)
+        {
+            audioManager.PlaySong(greenSong, 0);
+        }
+        else
+        {
+           audioManager.PlaySong(startSong, 0); 
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
         if (player.hasGreen)
         {
             terrain.treeBillboardDistance = 400;
         }
         else
         {
-            terrain.treeBillboardDistance = 0;
+            terrain.treeBillboardDistance = 5;
         }
+        
     }
+
+
 
     void OnTriggerEnter(Collider other)
     {
@@ -29,6 +39,8 @@ public class GreenTrigger : MonoBehaviour
         {
             Debug.Log("Player has picked up green paint");
             player.hasGreen = true;
+            audioManager.PlaySong(greenSong,0);
+            terrain.treeBillboardDistance = 400;
             Debug.Log("Player should now have green and can climb.");
         }
         gameObject.SetActive(false);
