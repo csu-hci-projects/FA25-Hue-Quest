5ec49c6084b8c7076857b81b3825938969198473
Prajjol Neupae <prajjol2014@gmail.com>
2025-12-07 17:45:34 -0700

    added bools to MainManager

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -17,6 +17,12 @@ public class MainManager : MonoBehaviour
             return;
         }
         instance = this;
+        instance.hasBlue = PlayerPrefs.GetInt("BlueUnlock", 0) == 1;
+        instance.hasRed = PlayerPrefs.GetInt("RedUnlock", 0) == 1;
+        instance.hasGreen = PlayerPrefs.GetInt("GreenUnlock", 0) == 1;
+        instance.hasYellow = PlayerPrefs.GetInt("YellowUnlock", 0) == 1;
+        instance.hasPurpule = PlayerPrefs.GetInt("PurpleUnlock", 0) == 1;
+        instance.hasOrange = PlayerPrefs.GetInt("OrangeUnlock", 0) == 1;
         DontDestroyOnLoad(gameObject);
     }
 }
